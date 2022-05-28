676cec20f48c13e4a9682b8740d37add5f1242a8
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2022-05-28 17:23:28 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-
-
-import './screens/loginscreen.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
-import './screens/home.dart';
+import './screens/ProfilePage.dart';
+import './screens/loginscreen.dart';
+import './screens/tempscreen.dart';
+import './screens/EventList.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,10 +16,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Email And Password Login',
+      title: 'Splash Screen',
       theme: ThemeData(primaryColor: Colors.black, fontFamily: 'JosefinSans'),
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
+      home: MyHomePage(),
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(
+            height: 350,
+          ),
+          CircleAvatar(
+            radius: 100.0,
+            backgroundColor: Colors.white,
+            child: ClipRRect(
+              child: Image.asset('assets/images/logo1.png'),
+              borderRadius: BorderRadius.circular(100.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
